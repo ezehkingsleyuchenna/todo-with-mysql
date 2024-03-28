cfc05e86e5b15c8b8b346a9827bf274eb7e6a0b6
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-03-28 15:14:01 +0100

    Update button action

--- resources/views/components/button-action.blade.php
+++ resources/views/components/button-action.blade.php
@@ -12,16 +12,15 @@
         {{ $attributes->class(['flex w-full' => $full, 'inline-flex' => ! $full, 'justify-center items-center space-x-2'])
                         ->merge(['type' => 'submit']) }}
         wire:loading.attr="disabled"
+        wire:loading.class="opacity-50"
         :$color
     >
         <span wire:loading @if($wireTarget) wire:target="{{ $wireTarget }}" @endif>
-            <x-icons.spinner sizes="w-3 h-3" />
+            <x-icons.spinner sizes="w-4 h-4" />
         </span>
         <span>{{ $label }}</span>
-        <span>
-            @if($icon)
-                <x-icons.user-plus class="w-6 h-6" />
-            @endif
+        <span wire:loading.remove @if($wireTarget) wire:target="{{ $wireTarget }}" @endif>
+            {{ $icon }}
         </span>
     </x-button>
 </div>
