23f6906f22bfa065ab9e2d14439b7da446d4b767
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-03-28 14:19:17 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+/** @typeof {import('tailwindcss').defaultTheme} defaultTheme */
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+    mode: 'jit',
+    content: [
+        './app/Livewire/**/*.php',
+        './app/Models/**/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+    ],
+    darkMode: 'class', // or 'media' or 'class'
+    theme: {
+        fontFamily: {
+            'sans': ['Helvetica', 'Arial', 'sans-serif'],
+            'heading': ['Helvetica']
+        },
+        extend: {
+            fontSize: {
+                heading: '34px',
+                subheading: '22px'
+            },
+            textOpacity: ['dark'],
+            colors: {
+                transparent: 'transparent',
+                white: '#ffffff',
+                black: '#000000',
+            },
+        },
+    },
+    variants: {
+        extend: {
+            backgroundColor: ['checked'],
+            borderColor: ['checked'],
+        }
+    },
+    plugins: [
+        require('@tailwindcss/forms')({
+            strategy: 'class',
+        }),
+    ],
 }
-
