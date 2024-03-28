ef4551543184612941521ff532e7fb41578e1420
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-03-28 14:26:07 +0100

    Update Login.php

--- app/Livewire/Login.php
+++ app/Livewire/Login.php
@@ -2,10 +2,28 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Login extends Component
 {
+    #[Validate(['required', 'email', 'exists:users,email'])]
+    public string $email;
+    #[Validate(['required', 'string'])]
+    public string $password;
+
+    public function login()
+    {
+        $this->validate();
+//        check user
+        if (! auth()->attempt($this->only('email', 'password'), true))
+            return $this->addError('email', trans('auth.failed'));
+        $user = auth()->user();
+//        redirect
+        $this->redirect(route('list'), true);
+        return true;
+    }
+
     public function render()
     {
         return view('livewire.login');
