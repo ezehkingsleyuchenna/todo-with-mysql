bf08f93167a260856c5e5084cbe906605a067b62
Ezeh Kingsley Uchenna <79694452+ezehkingsleyuchenna@users.noreply.github.com>
2024-03-28 14:24:51 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth')->group(function () {
-    Route::get('/', \App\Livewire\Index::class)->name('index');
-    Route::get('/logout', function () {
-        auth()->logout();
-        return redirect()->route('login');
-    })->name('logout');
+Route::middleware(\App\Http\Middleware\Added\Layout::class)->group(function () {
+    Route::middleware('auth')->group(function () {
+        Route::get('/', \App\Livewire\Index::class)->name('index');
+        Route::get('/logout', function () {
+            auth()->logout();
+            return redirect()->route('login');
+        })->name('logout');
+    });
+
+
+    Route::get('/login', \App\Livewire\Login::class)->name('login');
+    Route::get('/signup', \App\Livewire\Signup::class)->name('signup');
 });
 
 
-Route::get('/login', \App\Livewire\Login::class)->name('login');
-Route::get('/signup', \App\Livewire\Signup::class)->name('signup');
